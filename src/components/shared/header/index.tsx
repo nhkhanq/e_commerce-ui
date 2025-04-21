99c99722d1994124bdbb64cc08fe3f2db83f1dcf
NHKhanq <nhkhang061203@gmail.com>
2025-04-21 21:59:02 +0700

    feat: add shadcn; header, footer compoments

---
+++ src/components/shared/header/index.tsx
@@ -0,0 +1,11 @@
+import { APP_NAME } from "@/lib/constants";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div>Header</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = import.meta.env.VITE_APP_NAME || "Green Shop";
+export const APP_DESCRIPTION =
+  import.meta.env.VITE_APP_DESCRIPTION ||
+  "A website offering eco-friendly and sustainable food products.";
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || "http://localhost:5173";
